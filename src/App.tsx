49fae9fe30d4ebf89ed90aecf08d83f6c85665cb
Park Lu <chochinlu@gmail.com>
2024-06-02 10:43:15 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Test Telegram Mini App (Vite + React)</h1>
+      <h1>Test Telegram Mini App</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
